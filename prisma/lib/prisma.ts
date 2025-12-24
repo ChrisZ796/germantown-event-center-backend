1b3a6e0a783d4e5829681d3f4652c1afb56640f0
ChrisZ-360 <chrisz.wshs@gmail.com>
2025-12-24 14:51:47 -0600

    feat:changed ssl

--- prisma/lib/prisma.ts
+++ prisma/lib/prisma.ts
@@ -7,7 +7,11 @@ const adapter = new PrismaMariaDb({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME, connectionLimit:5
+    database: process.env.DATABASE_NAME, 
+    connectionLimit:10,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 const prisma = new PrismaClient({ adapter});
 
