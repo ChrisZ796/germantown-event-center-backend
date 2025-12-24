e624265b08eb7f067b539c058e169a83a01e725c
ChrisZ-360 <chrisz.wshs@gmail.com>
2025-12-24 14:09:05 -0600

    feat:cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = 8080
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:"https://germantown-event-center.vercel.app", methods:["GET", "POST", "PUT", "DELETE", "PATCH"], credentials:true}))
 
 // Create a new user
 app.post('/users', async (req, res) => {
