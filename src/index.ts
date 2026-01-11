7339c66ebb5166dabba10fee1bebb581f8f8b6c7
ChrisZ-360 <chrisz.wshs@gmail.com>
2026-01-11 09:34:38 -0600

    feat: route spec

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ const port = 8080
 
 app.use(express.json())
 app.use(cors({origin:"https://germantown-event-center.vercel.app", methods:["GET", "POST", "PUT", "DELETE", "PATCH"], credentials:true}))
+app.use((req, res, next) => {
+console.log(`Incoming request: ${req.method} ${req.url}`);
+next();
+});
 
 dotenv.config()
 
