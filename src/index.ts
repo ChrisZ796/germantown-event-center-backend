a9f2be2f5641474fd192119d6c4601942c2ac547
ChrisZ-360 <chrisz.wshs@gmail.com>
2026-02-14 03:43:11 -0600

    local testing

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const port = 8080
 
 app.use(express.json())
 //app.use(cors({origin:"https://germantown-event-center.vercel.app", methods:["GET", "POST", "PUT", "DELETE", "PATCH"], credentials:true}))
-app.use(cors({origin:"http://localhost:5173/", methods:["GET", "POST", "PUT", "DELETE", "PATCH"], credentials:true}))
+app.use(cors({origin:"http://localhost:5173", methods:["GET", "POST", "PUT", "DELETE", "PATCH"], credentials:true}))
 app.use((req, res, next) => {
 console.log(`Incoming request: ${req.method} ${req.url}`);
 next();
