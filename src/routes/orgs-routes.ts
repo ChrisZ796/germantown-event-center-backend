20a228966b77048b41adfaef179f478b56037766
ChrisZ-360 <chrisz.wshs@gmail.com>
2026-02-14 08:24:48 -0600

    fixed some routing endpoints

--- src/routes/orgs-routes.ts
+++ src/routes/orgs-routes.ts
@@ -4,10 +4,10 @@ import { OrgsController } from '../controllers/orgs-controller'
 const router = express.Router()
 const orgsControllerInstance = new OrgsController()
 
-router.post('/orgs', async(req, res) => orgsControllerInstance.createOrg(req, res))
-router.get('/orgs', async(req, res) => orgsControllerInstance.getAllOrgs(req, res))
-router.get('/orgs/:id', async(req, res) => orgsControllerInstance.getOrgInfo(req, res))
-router.patch('/orgs/:id', async(req, res) => orgsControllerInstance.updateOrg(req, res))
-router.delete('/orgs/:id', async(req, res) => orgsControllerInstance.deleteOrg(req, res))
+router.post('/', async(req, res) => orgsControllerInstance.createOrg(req, res))
+router.get('/', async(req, res) => orgsControllerInstance.getAllOrgs(req, res))
+router.get('/:id', async(req, res) => orgsControllerInstance.getOrgInfo(req, res))
+router.patch('/:id', async(req, res) => orgsControllerInstance.updateOrg(req, res))
+router.delete('/:id', async(req, res) => orgsControllerInstance.deleteOrg(req, res))
 
 export default router
